1122babe52695dcc8f96046729b24b18422a91cf
Giuseppe-Dicosmo <giuseppedicosmo530@gmail.com>
2022-03-15 17:59:25 +0100

    .

--- src/components/mainContent.vue
+++ src/components/mainContent.vue
@@ -2,24 +2,23 @@
   <div>
     <ul>
       <listaFilm v-for="film in movies" :key="film.id" />
-      {{film.title}}
+      {{film.original_title}}
     </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import listaFilm from "./listaFilm.vue"
+import listaFilm from "./listaFilm.vue";
 
 export default {
   name: "mainContent",
   components: {
-    listaFilm
+    listaFilm,
   },
   data() {
     return {
       movies: [],
-
     };
   },
   props: {
@@ -29,11 +28,19 @@ export default {
     },
   },
   methods: {
+    filterfilm() {
+      if (this.parola === "") {
+        return this.movies;
+      }
+    },
     metodo1: function () {
       axios
-        .get("https://api.themoviedb.org/3/movie/550?api_key=04cbd33b4b9f5d7abe1052e820d20b94")
+        .get(
+          "https://api.themoviedb.org/3/movie/550?api_key=04cbd33b4b9f5d7abe1052e820d20b94"
+        )
         .then((res) => {
           console.log("res.data", res.data);
+          this.movies = res.data;
         })
         .catch((error) => {
           console.log("error.data", error.response);
@@ -42,7 +49,7 @@ export default {
   },
   created() {
     this.metodo1();
-  }
+  },
 };
 </script>
 
